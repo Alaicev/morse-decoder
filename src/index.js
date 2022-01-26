69e7a9d53bc10c5b4ebc5e784e3f1ca00d1a176b
Alaicev <altarev123456@mail.ru>
2022-01-27 01:58:35 +0400

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let simbol = "";
+    let number = ""
+    let rezult = "";
+    let key = ""
+    
+    for (let i = 0; i < expr.length; i+=10) {
+        simbol = expr.slice(i, i+10);
+        console.log(simbol)
+        if(simbol === "**********") {
+            rezult += " ";
+        } else {
+        for(let j = 0; j < simbol.length; j+=2) {
+            number = simbol.slice(j, j+2);
+            if(number === "10"){
+                key+= ".";
+            }
+            if(number === "11") {
+                key += "-";
+            }
+            console.log(key)
+        }
+        rezult+=MORSE_TABLE[key];
+        key="";
+    }
+    }
+    return rezult
+    }
 
 module.exports = {
     decode
